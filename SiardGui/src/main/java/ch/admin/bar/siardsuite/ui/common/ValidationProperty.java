58d1d0101edd7da2ca94f921482464de4a7db1a3
hsthom <useforwildworld@gmail.com>
2025-01-06 14:34:11 +0900

    추출 메타 정보 입력 시 Time of Origin 선택형(콤보 박스 등)으로 개선

--- SiardGui/src/main/java/ch/admin/bar/siardsuite/ui/common/ValidationProperty.java
+++ SiardGui/src/main/java/ch/admin/bar/siardsuite/ui/common/ValidationProperty.java
@@ -3,29 +3,50 @@ package ch.admin.bar.siardsuite.ui.common;
 import ch.admin.bar.siardsuite.util.I18n;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Control;
+import javafx.scene.control.ComboBox;
 
 public class ValidationProperty {
-    private final TextField field;
+    private final Control field; // TextField나 ComboBox 등 다양한 컨트롤을 수용
+    //private final TextField field;
     private final Label validationMsgField;
     private final String validationMsg;
     private boolean isValid = true;
 
-    public ValidationProperty(TextField field, Label validationMsgField, String validationMsg) {
+    public ValidationProperty(Control field, Label validationMsgField, String validationMsg) {
         this.field = field;
         this.validationMsgField = validationMsgField;
         this.validationMsg = validationMsg;
     }
 
     public boolean validate() {
-        if (this.field.getText().isEmpty()) {
-            I18n.bind(this.validationMsgField.textProperty(), this.validationMsg);
-            this.validationMsgField.setVisible(true);
-            this.isValid = false;
+        if (field instanceof TextField) {
+            // TextField 검증
+            TextField textField = (TextField) field;
+            if (textField.getText() == null || textField.getText().isEmpty()) {
+                I18n.bind(this.validationMsgField.textProperty(), this.validationMsg);
+                this.validationMsgField.setVisible(true);
+                this.isValid = false;
+            } else {
+                this.validationMsgField.setVisible(false);
+            }
+        } else if (field instanceof ComboBox) {
+            // ComboBox 검증
+            ComboBox<?> comboBox = (ComboBox<?>) field;
+            if (comboBox.getValue() == null) {
+                I18n.bind(this.validationMsgField.textProperty(), this.validationMsg);
+                this.validationMsgField.setVisible(true);
+                this.isValid = false;
+            } else {
+                this.validationMsgField.setVisible(false);
+            }
         }
         return this.isValid;
     }
 
+
     public void reset() {
         isValid = true;
+        this.validationMsgField.setVisible(false);
     }
 }
